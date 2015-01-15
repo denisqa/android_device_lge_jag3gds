140f0500a1ab70f4fd408616492332bf3a303f66
denisqa <lionden2008@rambler.ru>
2015-01-15 21:15:31 +0200

    Update fscheck.sh

--- rootdir/fscheck.sh
+++ rootdir/fscheck.sh
@@ -4,8 +4,8 @@ FSTYPE=`/sbin/bbx blkid /dev/block/mmcblk0p34 | /sbin/bbx cut -d ' ' -f3 | /sbin
 
 if [ "$FSTYPE" == "ext4" ]
 then
-  /sbin/bbx rm /fstab_f2fs.jagnm
+  /sbin/bbx rm /fstab_f2fs.jag3gds
 else
-  /sbin/bbx cp -f /fstab_f2fs.jagnm /etc/recovery.fstab
-  /sbin/bbx mv -f /fstab_f2fs.jagnm /fstab.jagnm
+  /sbin/bbx cp -f /fstab_f2fs.jag3gds /etc/recovery.fstab
+  /sbin/bbx mv -f /fstab_f2fs.jag3gds /fstab.jag3gds
 fi
